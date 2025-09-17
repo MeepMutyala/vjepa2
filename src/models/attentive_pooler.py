6f11bb708ee542acade7700dd4ae98b56e4145d2
Apuroop Mutyala <apumutyala@gmail.com>
2025-09-17 12:54:13 -0400

    reverting to original to try messing with init.py in external_models base

--- src/models/attentive_pooler.py
+++ src/models/attentive_pooler.py
@@ -9,13 +9,13 @@ import math
 import torch
 import torch.nn as nn
 
-# from src.models.utils.modules import Block, CrossAttention, CrossAttentionBlock
-# from src.utils.tensors import trunc_normal_
+from src.models.utils.modules import Block, CrossAttention, CrossAttentionBlock
+from src.utils.tensors import trunc_normal_
 # from .utils.modules import Block, CrossAttention, CrossAttentionBlock
 # from ..utils.tensors import trunc_normal_
 
-from models.utils.modules import Block, CrossAttention, CrossAttentionBlock
-from utils.tensors import trunc_normal_
+# from models.utils.modules import Block, CrossAttention, CrossAttentionBlock
+# from utils.tensors import trunc_normal_
 
 
 class AttentivePooler(nn.Module):
